fa5e50cd7768b681d3490eea9b50ba492901f810
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-05-11 20:44:05 +0300

    Main

--- service/forms.py
+++ service/forms.py
@@ -1,9 +1,48 @@
-from django.forms import ModelForm
+from datetime import datetime
 
-from service.models import Client
+from django.forms import ModelForm
+from django import forms
+from service.models import Client, Order, Pizza, Payment, Courier, Chef, Delivery, Feedback, Promo
 
 
 class SearchFirst(ModelForm):
     class Meta:
         model = Client
         fields = ['client_order', 'phone_number']
+
+class SearchSecond(ModelForm):
+    class Meta:
+        model = Order
+        fields = ['courier_order', 'delivery_address']
+class SearchThird(ModelForm):
+    class Meta:
+        model = Pizza
+        fields = ['pizza_chef', 'name']
+class SearchFourth(forms.Form):
+    promo_payment = forms.CharField(max_length=100, label='Промокод')
+    payment_date = forms.DateField(input_formats=['%d.%m.%Y'], label='Дата оплати')
+
+class SearchFifth(ModelForm):
+    class Meta:
+        model = Courier
+        fields = ['delivery_courier', 'first_name']
+class SearchSixth(ModelForm):
+    class Meta:
+        model = Chef
+        fields = ['chef_order', 'first_name']
+class SearchSeventh(forms.Form):
+    payment_delivery = forms.CharField(max_length=100, label='Оплата')
+    delivery_date = forms.DateField(input_formats=['%d.%m.%Y'], label='Дата доставки')
+
+class SearchEighth(ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['client_feedback', 'feedback_date']
+class SearchNinth(forms.Form):
+    promo_order = forms.CharField(max_length=100, label='Замовлення')
+    promo_name = forms.CharField(max_length=100, label='Назва промокода')
+    class Meta:
+        model = Promo
+        fields = ['', 'promo_name']
+
+
--- service/views.py
+++ service/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 
-from service.forms import SearchFirst
-from service.models import Client
+from service.forms import SearchFirst, SearchSecond, SearchThird, SearchFourth, SearchFifth, SearchSixth, SearchSeventh, \
+    SearchEighth, SearchNinth
+from service.models import Client, Order, Pizza, Payment, Courier, Chef, Delivery, Feedback, Promo
 
 
 # Create your views here.
 def index(request):
     return render(request, 'service/index.html')
-
-#birth_date
-
 
 def first(request):
     form = SearchFirst()
@@ -24,32 +22,98 @@ def first(request):
 
 
 def second(request):
-    return render(request, 'service/second.html')
+    form = SearchSecond()
+    orders = Order.objects.all()
+    if request.method == "POST":
+        form = SearchSecond(request.POST)
+        if form.is_valid():
+            delivery_address = form.cleaned_data['delivery_address']
+            courier_order = form.cleaned_data['courier_order']
+            orders = Order.objects.filter(delivery_address=delivery_address, courier_order=courier_order)
+    return render(request, 'service/second.html', {'orders': orders, 'form': form})
 
 
 def third(request):
-    return render(request, 'service/third.html')
+    form = SearchThird()
+    pizzas = Pizza.objects.all()
+    if request.method == "POST":
+        form = SearchThird(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            pizza_chef = form.cleaned_data['pizza_chef']
+            pizzas = Pizza.objects.filter(name=name, pizza_chef=pizza_chef)
+    return render(request, 'service/third.html', {'pizzas': pizzas, 'form': form})
 
 
 def fourth(request):
-    return render(request, 'service/fourth.html')
+    form = SearchFourth()
+    payments = Payment.objects.all()
+    if request.method == "POST":
+        form = SearchFourth(request.POST)
+        if form.is_valid():
+            payment_date = form.cleaned_data['payment_date']
+            promo_payment = form.cleaned_data['promo_payment']
+            payments = Payment.objects.filter(payment_date=payment_date, promo_payment=promo_payment)
+    return render(request, 'service/fourth.html', {'payments': payments, 'form': form})
 
 
 def fifth(request):
-    return render(request, 'service/fifth.html')
+    form = SearchFifth()
+    couriers = Courier.objects.all()
+    if request.method == "POST":
+        form = SearchFifth(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            delivery_courier = form.cleaned_data['delivery_courier']
+            couriers = Courier.objects.filter(first_name=first_name, delivery_courier=delivery_courier)
+    return render(request, 'service/fifth.html', {'couriers': couriers, 'form': form})
 
 
 def sixth(request):
-    return render(request, 'service/sixth.html')
+    form = SearchSixth()
+    chefs = Chef.objects.all()
+    if request.method == "POST":
+        form = SearchSixth(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            chef_order = form.cleaned_data['chef_order']
+            chefs = Chef.objects.filter(first_name=first_name, chef_order=chef_order)
+    return render(request, 'service/sixth.html', {'chefs': chefs, 'form': form})
 
 
 def seventh(request):
-    return render(request, 'service/seventh.html')
+    form = SearchSeventh()
+    deliveries = Delivery.objects.all()
+    if request.method == "POST":
+        form = SearchSeventh(request.POST)
+        if form.is_valid():
+            delivery_date = form.cleaned_data['delivery_date']
+            payment_delivery = form.cleaned_data['payment_delivery']
+            deliveries = Delivery.objects.filter(delivery_date=delivery_date, payment_delivery=payment_delivery)
+    return render(request, 'service/seventh.html', {'deliveries': deliveries, 'form': form})
+
+
 
 
 def eighth(request):
-    return render(request, 'service/eighth.html')
+    form = SearchEighth()
+    feedbacks = Feedback.objects.all()
+    if request.method == "POST":
+        form = SearchEighth(request.POST)
+        if form.is_valid():
+            feedback_date = form.cleaned_data['feedback_date']
+            client_feedback = form.cleaned_data['client_feedback']
+            feedbacks = Feedback.objects.filter(feedback_date=feedback_date, client_feedback=client_feedback)
+    return render(request, 'service/eighth.html', {'feedbacks': feedbacks, 'form': form})
 
 
 def ninth(request):
-    return render(request, 'service/ninth.html')
+    form = SearchNinth()
+    promos = Promo.objects.all()
+    if request.method == "POST":
+        form = SearchNinth(request.POST)
+        if form.is_valid():
+            promo_name = form.cleaned_data['promo_name']
+            promo_order = form.cleaned_data['promo_order']
+            promos = Promo.objects.filter(promo_name=promo_name, promo_order=promo_order)
+    return render(request, 'service/ninth.html', {'promos': promos, 'form': form})
